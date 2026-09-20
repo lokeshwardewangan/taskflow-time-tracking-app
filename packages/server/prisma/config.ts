425d44710ff73145b9b58031155e896acff764d2
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2026-09-20 20:07:25 +0530

    feat(auth): add name field mandatory

--- packages/server/prisma/config.ts
+++ packages/server/prisma/config.ts
@@ -1,9 +1,11 @@
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../src/generated/prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
-const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
